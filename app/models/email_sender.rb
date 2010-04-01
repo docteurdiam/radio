e9dbca32a486e26a7f4396d0faeec916e8a2d5d6
Olivier Kouame <olivier.kouame@gmail.com>
2010-04-01 01:50:52 +0100

    AlL

--- app/models/email_sender.rb
+++ app/models/email_sender.rb
@@ -2,6 +2,7 @@ class EmailSender < ActionMailer::Base
   def reminder_notification(name, email, radios, total)
     recipients  "#{name} <#{email}>"
     from        "olivier.kouame@gmail.com"
+    content_type    "text/html"
     subject     "Your reminder from radios"
     sent_on     Time.now
     body        :name => name, :radios => radios, :total => total
